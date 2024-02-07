c57ac09ecb075ed4d0603f72f90c91243c569faa
karmel <karmel@pictimeprod.onmicrosoft.com>
2024-02-07 16:47:50 +0200

    Get selected images from jsons files

--- datasets/read_imgs_from_json.py
+++ datasets/read_imgs_from_json.py
@@ -25,19 +25,23 @@ def read_mongoextjson_file(filename):
         return data
 
 # Read images ids and spreads number of gallery for the albumn and order of images
-gallery_id = 23320463
-data = read_mongoextjson_file(f'json_files/{gallery_id}.json')
-number_spread = data['countCompositions']
-imges_listof_dict = data['placementsImg']
-imges_ids =[img['photoId'] for img in imges_listof_dict]
-print(f"Number of spreads for Album Number gallery {gallery_id}: {number_spread}")
-print(imges_ids)
-
-# copy images pics that selected to new folder
-for i, img_id in enumerate(imges_ids):
-    img_path = os.path.join(f'./album_galleries/{gallery_id}', f'{img_id}.jpg')
-    if not os.path.exists(img_path):
-        print(f"Image number {img_id} is not exists in gallery {gallery_id}")
+galleries = [23320463,26065526,27314637,27807822]
+for gallery_id in galleries:
+    if not os.path.exists(f'json_files/{gallery_id}.json'):
+        print("Json file for {gallery_id} is not exists!")
         continue
-    os.makedirs(f"./selected_imges/{gallery_id}", exist_ok=True)
-    shutil.copy(img_path,f"./selected_imges/{gallery_id}/{img_id}_{i}.jpg")
+    data = read_mongoextjson_file(f'json_files/{gallery_id}.json')
+    number_spread = data['countCompositions']
+    imges_listof_dict = data['placementsImg']
+    imges_ids =[(img['photoId'],img['compositionId'], img['placementImgId']) for img in imges_listof_dict]
+    print(f"Number of spreads for Album Number gallery {gallery_id}: {number_spread}")
+    print(f'Number of images for the album is {len(imges_ids)} : {imges_ids}')
+
+    # copy images pics that selected to new folder
+    for i, (img_id,spread_id, place_id) in enumerate(imges_ids):
+        img_path = os.path.join(f'./album_galleries/{gallery_id}', f'{img_id}.jpg')
+        if not os.path.exists(img_path):
+            print(f"Image number {img_id} is not exists in gallery {gallery_id}")
+            continue
+        os.makedirs(f"./selected_imges/{gallery_id}", exist_ok=True)
+        shutil.copy(img_path,f"./selected_imges/{gallery_id}/{img_id}_S{spread_id}_O{place_id}.jpg")
