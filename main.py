6c87c06456d6b720f543729c580fa8f70d081768
karmel <karmel@pictimeprod.onmicrosoft.com>
2025-03-27 12:56:38 +0100

    Combine all files together to main file

---
+++ main.py
@@ -0,0 +1,385 @@
+import os
+import json
+import torch
+import warnings
+import numpy as np
+
+import pandas as pd
+from typing import List, Union
+from datetime import datetime
+
+from ptinfra import intialize, get_logger
+from ptinfra.pt_queue import  MessageQueue, MemoryQueue, RoundRobinReader
+from ptinfra.config import get_variable
+from ptinfra.stage import Stage
+from ptinfra.pt_queue import QReader, QWriter, Message
+from ptinfra import  AbortRequested
+from ptinfra.azure.pt_file import PTFile
+
+from utils.clustering_time import cluster_by_time
+from utils.cover_image import process_non_wedding_cover_image, process_wedding_cover_end_image, get_cover_end_layout
+from utils.time_proessing import process_image_time
+from src.album_processing import start_processing_album
+from utils.parallel_methods import parallel_content_processing
+from utils.album_tools import assembly_output
+from utils.parser import CONFIGS
+from utils.request_processing import read_messages
+from utils.parser import CONFIGS
+
+if os.environ.get('PTEnvironment') == 'dev' or os.environ.get('PTEnvironment') is None:
+    os.environ['ConfigServiceURL'] = 'https://devqa.pic-time.com/config/'
+
+
+warnings.filterwarnings('ignore')
+np.random.seed(42)
+torch.manual_seed(42)
+os.environ["PYTHONHASHSEED"] = "42"
+os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+torch.set_num_threads(1)
+
+read_time_list = list()
+processing_time_list = list()
+processing_scores_time_list = list()
+storing_time_list = list()
+retorting_time_list = list()
+general_time_list = list()
+
+class ReadStage(Stage):
+    def __init__(self, in_q: QReader = None, out_q: QWriter = None, err_q: QWriter = None,
+                 logger=None):
+        super().__init__('ReadStage', self.read_messages, in_q, out_q, err_q, batch_size=1, max_threads=2)
+        self.logger = logger
+        self.queries_file = CONFIGS['queries_file']
+        self.products_json = PTFile(CONFIGS['products_json_location'])
+        fileBytes = self.products_json.read_blob()
+        self.products_json = json.loads(fileBytes.decode('utf-8'))
+
+        self.architect_base = CONFIGS['architect_base']
+
+        self.design_pack_base = CONFIGS['design_pack_base']
+
+
+    def read_messages(self, msgs: Union[Message, List[Message], AbortRequested]):
+        if isinstance(msgs, AbortRequested):
+            self.logger.info("Abort requested")
+            return []
+
+        messages = msgs if isinstance(msgs, list) else [msgs]
+
+        for msg in msgs:
+            productId = msg.conten['productId']
+            product_list = self.products_json['products']
+            for product_dict in product_list:
+                if product_dict['productId'] == productId:
+                    break
+
+            packageTypeId = product_dict['packageTypeId']
+            productGroupId = product_dict['productGroupId']
+
+            design_package = os.path.join(self.design_pack_base, f'pack_{packageTypeId}.json.txt')
+            design_package = PTFile(design_package)
+            fileBytes = design_package.read_blob()
+            design_package = json.loads(fileBytes.decode('utf-8'))
+
+            msg.designs = design_package
+
+            architect_package = os.path.join(self.architect_base, f'{productGroupId}/architect2.json.en-us.txt')
+            architect_package = PTFile(architect_package)
+            fileBytes = architect_package.read_blob()
+            architect_package = json.loads(fileBytes.decode('utf-8'))
+
+            rules = architect_package['planningRules']
+            msg.defaultPackageStyleId = rules['defaultPackageStyleId']
+
+            compositions = rules['compositions']
+            cover = None
+            for composition in compositions:
+                if composition['name']=='cover':
+                    cover = composition
+                    break
+
+            msg.cover = False
+            if cover is not None:
+                msg.cover=True
+                msg.cover_designs = cover['designIds']
+
+            first = None
+            for composition in compositions:
+                if composition['name'] == 'first':
+                    first = composition
+                    break
+
+            msg.first = False
+            if first is not None:
+                msg.first = True
+                msg.first_designs = first['designIds']
+
+            last = None
+            for composition in compositions:
+                if composition['name'] == 'last':
+                    last = composition
+                    break
+
+            msg.last = False
+            if last is not None:
+                msg.last = True
+                msg.last_designs = last['designIds']
+
+            any_page = None
+            for composition in compositions:
+                if composition['name'] == 'any page':
+                    any_page = composition
+                    break
+            if any_page is None:
+                self.logger.error('no designs found for any page')
+                msg.error = Exception('no designs found for any page')
+            else:
+                msg.designs = any_page['designIds']
+
+
+        start = datetime.now()
+        #Read messages using a helper function
+        try:
+            messages = read_messages(messages, self.queries_file, self.logger)
+        except Exception as e:
+            self.logger.error(f"Error reading messages: {e}")
+            return []
+
+        handling_time = (datetime.now() - start) / len(messages) if messages else 0
+        self.logger.info(f"READING Stage for {len(messages)} messages. Average time: {handling_time}")
+        return messages
+
+class ProcessStage(Stage):
+    def __init__(self, in_q: QReader = None, out_q: QWriter = None, err_q: QWriter = None,
+                 logger=None):
+        super().__init__('ProcessingStage', self.process_message, in_q, out_q, err_q, batch_size=1, max_threads=2,
+        batch_wait_time=5)
+        self.logger = logger
+
+    def process_message(self, msgs: Union[Message, List[Message]]):
+        # check if its single message or list
+        messages = msgs if isinstance(msgs, list) else [msgs]
+        whole_messages_start = datetime.now()
+
+        Spread_score_threshold_params = [0.01,0.5,0.05,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01]
+        Partition_score_threshold_params = [100,100,100,200,150,100,100,100,100,100,100,100,100]
+        Maxm_Combs_params = [1000,1000,1000,1000,1000,50,10,1000,1000,1000,1000,1000,1000]
+        MaxCombsLargeGroups_params = [100,100,100,100,100,100,100,20,5,100,100,100,100]
+        MaxOrientedCombs_params = [300,300,300,300,300,300,300,300,300,10,50,300,300]
+        Max_photo_groups_params = [12,12,12,12,12,12,12,12,12,12,12,8,5]
+
+        for i,message in enumerate(messages):
+            if i > 13:
+                i = 0
+
+            params = [Spread_score_threshold_params[i], Partition_score_threshold_params[i], Maxm_Combs_params[i],MaxCombsLargeGroups_params[i],MaxOrientedCombs_params[i],Max_photo_groups_params[i]]
+            print("Params for this Gallery are:", params)
+            try:
+                stage_start = datetime.now()
+                # Extract gallery photo info safely
+                df = message.content.get('gallery_photos_info', pd.DataFrame())
+                if df.empty:
+                    self.logger.error(f"Gallery photos info DataFrame is empty for message {message}")
+                    message.content['error'] = f"Gallery photos info DataFrame is empty for message {message}"
+                    continue
+
+                if "image_order" not in df.columns:
+                    self.logger.error(f"Missing 'image_order' column in DataFrame for message {message}")
+                    message.content['error'] = f"Missing 'image_order' column in DataFrame for message {message}"
+                    continue
+
+                # Sorting the DataFrame by "image_order" column
+                sorted_df = df.sort_values(by="image_order", ascending=False)
+
+                try:
+                    if message.content.get('is_wedding', True):
+                        processed_content_df = parallel_content_processing(sorted_df)
+                        processed_df = sorted_df.merge(processed_content_df[['image_id', 'cluster_context']],
+                                                       how='left', on='image_id')
+                        df, cover_end_images_ids, cover_end_imgs_df = process_wedding_cover_end_image(processed_df,
+                                                                                                      self.logger)
+                    else:
+                        df, cover_end_images_ids, cover_end_imgs_df = process_non_wedding_cover_image(sorted_df,
+                                                                                                      self.logger)
+                except Exception as e:
+                    self.logger.error(f"Error in parallel content processing: {e}")
+                    message.content['error'] = f"Error in parallel content processing: {e}"
+                    continue
+
+                cover_end_imgs_layouts = get_cover_end_layout(message.content['layouts_df'], self.logger)
+                if not cover_end_imgs_layouts:
+                    self.logger.error(f"No cover-end layouts found for message {message}")
+                    message.content['error'] = f"No cover-end layouts found for message {message}"
+                    continue
+
+                sorted_by_time_df, image_id2general_time = process_image_time(df)
+                df_time = cluster_by_time(sorted_by_time_df)
+
+                # to ignore the read only memory
+                df = pd.DataFrame(df_time.to_dict())
+
+                # Handle the processing time logging
+                try:
+                    start = datetime.now()
+                    album_result = start_processing_album(df, message.content['layouts_df'],
+                                                          message.content['layout_id2data'],
+                                                          message.content['is_wedding'],params, logger=self.logger)
+
+                    final_response = assembly_output(album_result, message, message.content['layouts_df'], df,
+                                                     cover_end_images_ids, cover_end_imgs_df, cover_end_imgs_layouts)
+
+                    if isinstance(album_result, str):  # Check if it's an error message, report it
+                        message.content['error'] = final_response
+                        continue
+
+                    message.content['album'] = final_response
+                    processing_time = datetime.now() - start
+
+                    self.logger.debug('Lay-outing time: {}.For Processed album id: {}'.format(processing_time,
+                                                                                              message.content.get(
+                                                                                                  'projectURL', True)))
+                    self.logger.debug(
+                        'Processing Stage time: {}.For Processed album id: {}'.format(datetime.now() - stage_start,
+                                                                                      message.content.get('projectURL',
+                                                                                                          True)))
+
+                except Exception as ex:
+                    self.logger.error('Exception while processing messages: {}.'.format(ex))
+                    message.content['error'] = ex
+                    continue
+
+            except Exception as e:
+                self.logger.error(f"Unexpected error in message processing: {e}")
+                message.content['error'] = f"Unexpected error in message processing: {e}"
+                continue
+
+        handling_time = (datetime.now() - whole_messages_start) / len(messages) if messages else 0
+        self.logger.debug('Average Processing Stage time: {}. For : {} messages '.format(handling_time, len(messages)))
+        return msgs
+
+
+
+class ReportStage(Stage):
+    def __init__(self, in_q: QReader = None, out_q: QWriter = None, err_q: QWriter = None,
+                 logger=None):
+        super().__init__('ReportMessage', self.report_message, in_q, out_q, err_q, batch_size=1, max_threads=1)
+        self.global_start_time = datetime.now()
+        self.global_number_of_msgs = 0
+        self.number_of_reports = 0
+        self.logger = logger
+
+    def print_time_summary(self, period=10):
+        self.number_of_reports += 1
+        if self.number_of_reports % period != 0:
+            return
+
+        def avg_time(datetimes):
+            if len(datetimes) == 0:
+                return 1000000
+            total = sum(dt.seconds * 1000000 + dt.microseconds for dt in datetimes)
+            avg = total / len(datetimes)
+            avg = float(avg) / 1000000
+            return avg
+
+        global_average = (datetime.now() - self.global_start_time) / self.global_number_of_msgs \
+            if self.global_number_of_msgs > 0 else 1000000
+
+        self.logger.debug('**********. '
+                          'Average processing time for last {} requests: '
+                          'Handling messages and data loading average time: {}. '
+                          'Processing average time: {}. '
+                          'Processing scores average time: {}. '
+                          'Storing average time: {}. '
+                          'Reporting average time: {}. '
+                          'General average time: {}. '
+                          '**********.'.format(self.global_number_of_msgs, avg_time(read_time_list),
+                                               avg_time(processing_time_list), avg_time(processing_scores_time_list),
+                                               avg_time(storing_time_list), avg_time(retorting_time_list),
+                                               global_average))
+
+    def report_one_message(self, one_msg):
+        if one_msg.error:
+            self.logger.debug('REPORT ERROR MESSAGE  {}.'.format(one_msg.error))
+
+    def report_message(self, msgs: Union[Message, List[Message]]):
+        start = datetime.now()
+        if isinstance(msgs, Message):
+            self.report_one_message(msgs)
+            msgs.delete()
+        elif isinstance(msgs, list):
+            for one_msg in msgs:
+                self.report_one_message(one_msg)
+                one_msg.delete()
+
+        reporting_time = (datetime.now() - start) / (len(msgs) if isinstance(msgs, list) else 1)
+        retorting_time_list.append(reporting_time)
+
+        # photo_ids = msgs.content['photoId'] if isinstance(msgs, Message) else [msg.content["photoId"] for msg in msgs]
+        # self.logger.debug('Deleted images: {}. Reporting time: {}.'.format(photo_ids, reporting_time))
+
+        self.global_number_of_msgs += len(msgs) if isinstance(msgs, list) else 1
+        self.print_time_summary()
+
+        return
+
+
+
+class MessageProcessor:
+    def __init__(self):
+        self.logger = get_logger(__name__, 'DEBUG')
+
+    def run(self):
+        settings_filename = os.environ.get('HostingSettingsPath',
+                                           '/ptinternal/pictures/hosting/ai_settings_audiobeat.json.txt')
+
+        intialize('AlbumDesigner', settings_filename)
+
+        private_key = get_variable('PtKey')
+        self.logger.debug('Private key: {}'.format(private_key))
+
+        try:
+            prefix = get_variable('PTEnvironment')
+        except:
+            prefix = 'dev'
+
+        input_queue = CONFIGS['collection_name']
+        print(prefix + input_queue)
+        if prefix == 'dev':
+            dev_queue = MessageQueue(prefix + input_queue, def_visibility=CONFIGS['visibility_timeout'],
+                                     max_dequeue_allowed=1000)
+            test_queue = MessageQueue('test' + input_queue, def_visibility=CONFIGS['visibility_timeout'],
+                                      max_dequeue_allowed=1000)
+            ep_queue = MessageQueue('ep' + input_queue, def_visibility=CONFIGS['visibility_timeout'],
+                                    max_dequeue_allowed=1000)
+            azure_input_q = RoundRobinReader([dev_queue, test_queue, ep_queue])
+        elif prefix == 'production':
+            self.logger.info('PRODUCTION environment set, queue name: ' + input_queue)
+            azure_input_q = MessageQueue(input_queue, def_visibility=CONFIGS['visibility_timeout'],
+                                         max_dequeue_allowed=1000)
+        else:
+            self.logger.info(prefix + ' environment, queue name: ' + prefix + input_queue)
+            azure_input_q = MessageQueue(prefix + input_queue, def_visibility=CONFIGS['visibility_timeout'],
+                                         max_dequeue_allowed=1000)
+
+        read_q = MemoryQueue(2)
+        process_q = MemoryQueue(2)
+        report_q = MemoryQueue(2)
+
+        read_stage = ReadStage(azure_input_q, read_q, report_q, logger=self.logger)
+        process_stage = ProcessStage(read_q, process_q, report_q, logger=self.logger)
+        report_stage = ReportStage(report_q, logger=self.logger)
+
+        report_stage.start()
+        process_stage.start()
+        read_stage.start()
+
+
+def main():
+    message_processor = MessageProcessor()
+    message_processor.run()
+
+
+if __name__ == '__main__':
+    main()
