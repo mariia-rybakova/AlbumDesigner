faf6a8442a896a02dffd7ab5ffd172d69abdfd2b
Mariia Rybakova <mariia@pic-time.com>
2026-02-12 16:00:55 +0200

    split-merge tools refactor

--- src/groups_operations/groups_splitting_merging.py
+++ src/groups_operations/groups_splitting_merging.py
@@ -28,52 +28,62 @@ SIMILAR_CLASSES_L2 = [
     ]
 
 
+def _normalize_feature(feature):
+    min_value = min(feature)
+    max_value = max(feature)
+
+    if min_value == max_value:
+        feature_normalized = [0] * len(feature)
+    else:
+        feature_normalized = [(value - min_value) / (max_value - min_value) for value in feature]
+    return feature_normalized
+
+
+def _clusterize(illegal_group, feature, n_clusters, size_min, size_max, count, silhouette = False, split_prefix = ''):
+    # Apply constrained K-Means clustering
+    clf = KMeansConstrained(
+        n_clusters=n_clusters,
+        size_min=size_min,
+        size_max=size_max,
+        random_state=0
+    )
+    labels = clf.fit_predict(feature)
+
+    # Estimate clustering quality
+    if silhouette:
+        silhouette_avg = silhouette_score(feature, labels)
+        if silhouette_avg < 0.15:
+            return None, None
+
+    # Assign cluster labels with a prefix or suffix to distinguish them from other groups
+    # Create new cluster labels with original context preserved
+    content_cluster_origin = illegal_group['cluster_context'].values[0]
+    new_labels = [f'{content_cluster_origin}_{split_prefix}{label}_{count}' for label in labels]
+
+    # Assign new cluster labels to the DataFrame
+    illegal_group.loc[:, 'cluster_context'] = new_labels
+    return content_cluster_origin, new_labels
+
+
 def split_illegal_group(illegal_group, count):
     illegal_group_features = illegal_group['embedding'].values.tolist()
     illegal_time_features = [time for time in illegal_group["general_time"]]
-
     cluster_labels = [cluster_label for cluster_label in illegal_group["cluster_label"]]
-    min_labels = min(cluster_labels)
-    max_labels = max(cluster_labels)
 
-    min_value = min(illegal_time_features)
-    max_value = max(illegal_time_features)
-
-    if min_labels == max_labels:
-        cluster_labels_nor = [0] * len(cluster_labels)
-    else:
-        cluster_labels_nor = [(label - min_labels) / (max_labels - min_labels) for label in cluster_labels]
-
-    if min_value == max_value:
-        time_features_nor = [0] * len(illegal_time_features)
-    else:
-        time_features_nor = [(time - min_value) / (max_value - min_value) for time in illegal_time_features]
-
-    combined_features = np.column_stack((illegal_group_features, time_features_nor,cluster_labels_nor))
+    cluster_labels_nor = _normalize_feature(cluster_labels)
+    time_features_nor = _normalize_feature(illegal_time_features)
+    combined_features = np.column_stack((illegal_group_features, time_features_nor, cluster_labels_nor))
 
     n_samples = len(combined_features)
+    n_clusters = 2
     size_min = max(1, n_samples // 4)
-    clf = KMeansConstrained(
-        n_clusters=2,
-        size_min=size_min,
-        size_max=len(combined_features),
-        random_state=0)
-    clf.fit_predict(combined_features)
+    size_max = n_samples
+    _, labels = _clusterize(illegal_group, combined_features, n_clusters, size_min, size_max, count,
+                         silhouette = True, split_prefix = '')
+    if labels is None:
+        return None, None
 
-    labels = clf.labels_
-    silhouette_avg = silhouette_score(combined_features, labels)
-
-    if silhouette_avg < 0.15:
-        return None,None
-
-    content_cluster_origin = illegal_group['cluster_context'].values[0]
-    labels = [f'{content_cluster_origin}_{label}_{count}' for label in labels]
-    label_counts = Counter(labels)
-    # Assign cluster labels with a prefix or suffix to distinguish them from other groups
-    # illegal_group['main_content_cluster'] = [f"{content_cluster_origin}_{label}" for label in labels]
-    illegal_group.loc[:,'cluster_context'] = [label for label in labels]
-
-    return illegal_group, label_counts
+    return illegal_group, Counter(labels)
 
 
 def _get_sizes(single_spread_size):
@@ -87,13 +97,13 @@ def _get_sizes(single_spread_size):
     elif single_spread_size >= 12:
         split_size = single_spread_size * 2
     else:
-        split_size = single_spread_size * 3
+        split_size = single_spread_size * 3 # #photos for 3 spreads
     return max_size_splits, split_size
 
 
 def _split_by_chunk_size(illegal_group, split_size, n_samples, count):
+    # If the split size is invalid or larger than the group, don't split
     if split_size <= 0 or split_size >= n_samples:
-        # If the split size is invalid or larger than the group, don't split
         return None, None
 
     # Create chunks of size `split_size`
@@ -116,20 +126,8 @@ def _split_with_time_clustering(illegal_group, n_clusters, size_min, size_max, c
         time_features = illegal_group["general_time"].values.reshape(-1, 1)
 
         # Apply constrained K-Means clustering on time
-        clf = KMeansConstrained(
-            n_clusters=n_clusters,
-            size_min=size_min,
-            size_max=size_max,
-            random_state=0
-        )
-        labels = clf.fit_predict(time_features)
-
-        # Create new cluster labels with original context preserved
-        content_cluster_origin = illegal_group['cluster_context'].values[0]
-        new_labels = [f'{content_cluster_origin}_split_{label}_{count}' for label in labels]
-
-        # Assign new cluster labels to the DataFrame
-        illegal_group.loc[:, 'cluster_context'] = new_labels
+        content_cluster_origin, _ = _clusterize(illegal_group, time_features, n_clusters, size_min, size_max, count,
+                                 silhouette = False, split_prefix = 'split_')
 
         # Sort groups by mean time for temporal ordering
         mean_times = illegal_group.groupby('cluster_context')['general_time'].mean()
@@ -196,31 +194,36 @@ def split_illegal_group_in_certain_point(illegal_group, split_points, count):
     return illegal_group, label_counts
 
 
-def merge_illegal_group(main_groups, illegal_group):
-    clusters_features = [group['embedding'].values.copy() for group in main_groups]
-
-    # Aggregate features within each group
-    group_features = [group.mean() if group.shape[0] > 1 else group[0] for group in clusters_features]
-    group_features_np = np.array(group_features)
+def _mean_or_first_element(feature):
+    if feature.shape[0] > 1:
+        return feature.mean()
+    return feature[0]
 
 
-    if illegal_group['embedding'].values.shape[0] > 1 :
-        illegal_group_features = illegal_group['embedding'].values.mean()
-    else:
-        illegal_group_features = illegal_group['embedding'].values[0]
+def _get_embedding_feature(illegal_group):
+    illegal_group_features = illegal_group['embedding'].values
+    illegal_group_features = _mean_or_first_element(illegal_group_features)
 
     if len(illegal_group_features.shape) == 1:
-        intded_group_fe = illegal_group_features.reshape(1, -1)
-    else:
-        intded_group_fe = illegal_group_features
+        return illegal_group_features.reshape(1, -1)
+    return illegal_group_features
 
+
+def merge_illegal_group(main_groups, illegal_group):
+    # Aggregate features within illegal group
+    intded_group_fe = _get_embedding_feature(illegal_group)
     inteded_group_time = illegal_group['general_time'].values.mean()
     intded_group_fe_with_time = np.column_stack((intded_group_fe, inteded_group_time))
 
-    main_groups_time_without_illegal = [group['general_time'].values.mean() for i, group in enumerate(main_groups)]
+    # Aggregate features within each group
+    clusters_features = [group['embedding'].values.copy() for group in main_groups]
+    group_features = [_mean_or_first_element(group) for group in clusters_features]
+    group_features_np = np.array(group_features)
+    main_groups_time_without_illegal = [group['general_time'].values.mean() for group in main_groups]
     groups_combined_features = np.column_stack((group_features_np, main_groups_time_without_illegal))
 
-    dist_to_illegal_group = pairwise_distances(intded_group_fe_with_time,groups_combined_features,
+    # Compute distances
+    dist_to_illegal_group = pairwise_distances(intded_group_fe_with_time, groups_combined_features,
                                                metric='cosine')
 
     # Find the index of the group with the minimum distance to illegal_group
@@ -361,39 +364,3 @@ def merge_illegal_group_by_time(main_groups, illegal_group, general_times_list, 
 
     # If no suitable group is found, return None
     return None, None
-
-    # intended_group_time = illegal_group['general_time'].values.mean()
-    # # Calculate minimum time differences for each main group
-    # time_differences = []
-    # for group in main_groups:
-    #     # Calculate time differences between illegal group mean and all times in current group
-    #     group_times = group['general_time'].values
-    #     time_diffs = np.abs(group_times - intended_group_time)
-    #     # Get the minimum difference to any image in the group
-    #     min_time_diff = np.min(time_diffs)
-    #     updated_time_diff = add_class_preference(illegal_group, group, min_time_diff)
-    #     time_differences.append(updated_time_diff)
-    #
-    # time_differences = np.array(time_differences)
-    # sorted_indices = np.argsort(time_differences)
-    #
-    # selected_cluster = None
-    # selected_time_difference = None
-    #
-    # # Try groups in order of temporal proximity
-    # for idx in sorted_indices:
-    #     selected_cluster = main_groups[idx]
-    #     len_combine_group = len(selected_cluster) + len(illegal_group)
-    #
-    #     # Check if the combination meets size requirements
-    #     if len_combine_group <= max_images_per_spread:
-    #         selected_time_difference = time_differences[idx]
-    #         break
-    #
-    # # If still no suitable group found, use the temporally closest group regardless of size
-    # if selected_time_difference is None and len(sorted_indices) > 0:
-    #     closest_idx = sorted_indices[0]
-    #     selected_cluster = main_groups[closest_idx]
-    #     selected_time_difference = time_differences[closest_idx]
-    #
-    # return selected_cluster, selected_time_difference
