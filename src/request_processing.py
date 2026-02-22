21d29322cc93b78ceec651a21131d6c7e95df15c
ziv-rotman <107126744+ziv-rotman@users.noreply.github.com>
2026-02-22 23:12:00 +0200

    qdrant read format to ndarray

--- src/request_processing.py
+++ src/request_processing.py
@@ -517,7 +517,7 @@ def read_messages(messages, project_status_collection, qdrant_client, logger):
                     clip_version = image_model_version
                     _msg.clip_version = clip_version
                     clip_df = pd.DataFrame([
-                        {"image_id": photo_id, "embedding": data}
+                        {"image_id": photo_id, "embedding": np.array(data)}
                         for photo_id, data in clip_dict.items()
                     ])
                     clip_df['model_version'] = image_model_version
